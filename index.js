29cbe8a98e7a275221b3ee528aaf1de9a45e4bb2
Hossam <hossamsaeedg99@yahoo.com>
2020-10-15 19:20:38 +0200

    Simple Node Example

---
+++ index.js
@@ -0,0 +1,20 @@
+var rect = {
+    perimeter: (x,y) => (2*(x+y)),
+    area: (x,y) => (x*y)
+};
+
+function solveRect(l,b) {
+    console.log("solution for l = " + l +"and b = " + b);
+
+    if (l<= 0 || b<=0) {
+        console.log("Dimensions should be greater than 0, l=" +l+ " b="+b);
+    } else {
+        console.log("Area is " + rect.area(l,b));
+        console.log("Perimeter is " + rect.perimeter(l,b));
+    }
+}
+
+solveRect(2,4);
+solveRect(3,5);
+solveRect(0,5);
+solveRect(-3,5);
